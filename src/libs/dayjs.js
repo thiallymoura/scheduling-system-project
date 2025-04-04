4ffb671dec02d1242870cd0ae7df6e1109cc4986
Thially Moura <thiallymoura@hotmail.com>
2025-04-04 09:25:52 -0300

    feat: Installation and configuration of webpack, webpack server, babel and the dayjs package

---
+++ src/libs/dayjs.js
@@ -0,0 +1,6 @@
+//Importando o dayjs
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br";
+
+dayjs.locale("pt-br"); // Definindo o idioma padrão para o dayjs
+
---
+++ src/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// Configuração do dayjs
+import "./libs/dayjs.js";
+
+// CSS
+import "./styles/global.css";
+import "./styles/form.css";
+import "./styles/schedule.css";
+
+
+// Testando a data
+import dayjs from "dayjs";
+console.log(dayjs().format("DD/MM/YYYY HH:mm"));
